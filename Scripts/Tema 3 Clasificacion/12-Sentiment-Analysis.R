a4bbfd543cbfd970e6c82648a738554a332c2990
Adipiz <88919946+Adipiz@users.noreply.github.com>
2022-02-10 12:26:30 -0600

    up

---
+++ Scripts/Tema 3 Clasificacion/12-Sentiment-Analysis.R
@@ -0,0 +1,11 @@
+
+# Análisis de sentimientos ------------------------------------------------
+
+install.packages(c("twitteR", "RColorBrewer","plyr", "ggplot", "devtools", "httr"))
+require(devtools)
+# install_url("https://cran.r-project.org/src/contrib/Archive/Rstem/Rstem_0.4-1")
+# Cargué el paquete senimentR
+
+
+library(sentimentr)
+
